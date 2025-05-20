8a6ffacd5f137c9f632e14e54d60f68b7118a6a9
babsontimie <139099122+babsontimie@users.noreply.github.com>
2025-05-20 23:15:58 +0100

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,8 +1,8 @@
 def add(x, y){
 
-  echo "Sum of ${x} and ${y} i  ${x+y}"
+  echo "Sum of ${x} and ${y} is  ${x+y}"
 }
 def mult(x, y){
 
-  echo "Multiplication of ${x} and ${y} i  ${x*y}"
+  echo "Multiplication of ${x} and ${y} is  ${x*y}"
 }
